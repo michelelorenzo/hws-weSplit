180af5cf7c3b409a24a89a8292045445ade2817a
Michele Lorenzo Miranda <michele.lorenzom@gmail.com>
2024-07-17 12:21:07 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Michele Lorenzo Miranda on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
